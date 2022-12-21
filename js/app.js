f50e61dd193d2abc12a91fc43830b5668cb0af68
MORSALIN05 <mdmsarker04@gmail.com>
2022-12-21 23:17:45 +0600

    spinner toggle display committed

--- js/app.js
+++ js/app.js
@@ -1,22 +1,31 @@
-const loadPhones = async () => {
-    const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
+const loadPhones = async (searchText) => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     const res = await fetch(url);
     const data = await res.json();
     displayPhones(data.data);
 }
+const divContainer = document.getElementById('phone-container');
 const displayPhones = phones => {
-    console.log(typeof (phones));
+    //console.log(typeof (phones));
     //console.log(phones);
-    const divContainer = document.getElementById('phone-container');
+    // if no phone fuond
+    const noPhoneFound = document.getElementById('no-phone-found');
+    if (phones.length === 0) {
+        noPhoneFound.classList.remove('d-none');
+    }
+    else {
+        noPhoneFound.classList.add('d-none');
+    }
+    phones = phones.slice(0, 20);
     phones.forEach(phone => {
         const div = document.createElement('div');
 
         div.classList.add('col');
         div.innerHTML = `
-        <div class="card">
+        <div class="card p-4">
         <img src="${phone.image}" class="card-img-top" alt="...">
         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
+            <h5 class="card-title">${phone.phone_name}</h5>
             <p class="card-text">This is a longer card with supporting text below as a natural
                 lead-in
                 to additional content. This content is a little bit longer.</p>
@@ -24,6 +33,28 @@ const displayPhones = phones => {
         `;
         divContainer.appendChild(div);
     })
+    spinnerDisplay(false);
 
 }
+divContainer.textContent = '';
+document.getElementById('btn-search').addEventListener('click', function () {
+    spinnerDisplay(true);
+    const searchField = document.getElementById('search-field');
+
+    const searchText = searchField.value;
+    loadPhones(searchText);
+    divContainer.textContent = '';
+
+})
+
 loadPhones();
+// spin loader function
+const spinnerDisplay = (isLoading) => {
+    const spinnerWorking = document.getElementById('spin-loader');
+    if (isLoading) {
+        spinnerWorking.classList.remove('d-none');
+    }
+    else {
+        spinnerWorking.classList.add('d-none');
+    }
+}
